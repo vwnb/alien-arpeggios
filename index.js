c79c05dac797cf96037eb6b6aa2d72d343b3e742
Ville Kemppainen <villekemppainen@Villes-MacBook-Pro.local>
2023-01-08 05:40:40 +0200

    Encapsulate everything

--- index.js
+++ index.js
@@ -5,113 +5,155 @@ if (process.env.NODE_ENV !== 'production') {
 const easymidi = require('easymidi');
 
 /**
- * Configuration
+ * Public cnfiguration
  */
-const bus = 'IAC Driver Bus 1';
-const lowerNote = 36;
-const upperNote = 72;
-
-if (easymidi.getOutputs().includes(bus)) {
-  console.log(easymidi.getOutputs());
-} else {
-  console.log('MIDI bus ' + bus + ' not available');
-  process.exit();
+class AlienArpeggiosConfiguration
+{
+  constructor()
+  {
+    this.bus       = 'IAC Driver Bus 1';
+    this.lowerNote = 36;
+    this.upperNote = 72;
+    
+    if (easymidi.getOutputs().includes(this.bus)) {
+      console.log(easymidi.getOutputs());
+    } else {
+      console.log('MIDI bus ' + this.bus + ' not available');
+      process.exit();
+    }
+    
+    this.output = new easymidi.Output(this.bus);
+  }
 }
 
-const output = new easymidi.Output(bus);
-
-var counter  = 0;
-
 /**
- * Note player
- *
- * @param {number} note - MIDI note from 0 to 127
+ * Private helper class
  */
-const playNote = (note) => {
-  output.send('noteon', {
-    note: Math.floor(note),
-    velocity: 127,
-    channel: 3
-  });
+class AlienArpeggiosHelper
+{
+  constructor(configuration = new AlienArpeggiosConfiguration) {
+    this.output  = configuration.output;
+    this.counter = 0;
+  }
 
-  setTimeout(() => {
-    output.send('noteoff', {
+  /**
+   * Note player
+   *
+   * @param {number} note - MIDI note from 0 to 127
+   */
+  playNote = (note) =>
+  {
+    this.output.send('noteon', {
+      note: Math.floor(note),
+      velocity: 127,
+      channel: 3
+    });
+
+    setTimeout(() => {
+      this.output.send('noteoff', {
         note: Math.floor(note),
         velocity: 127,
         channel: 3
       });
-  }, 50);
-}
- 
-/**
- * Sine wave oscillator
- *
- * @param {number} speed - wave period
- * @param {number} depth - wave amplitude
- *
- * @return {number}
- */
-const sinOscillator = (speed = 1, depth = 1) => {
-  return Math.sin(counter * speed) * depth
+    }, 50);
+  }
+  
+  /**
+   * Sine wave oscillator
+   *
+   * @param {number} speed - wave period
+   * @param {number} depth - wave amplitude
+   *
+   * @return {number}
+   */
+  sinOscillator = (speed = 1, depth = 1) =>
+  {
+    return Math.sin(this.counter * speed) * depth
+  }
+
+  /**
+   * Tangent wave oscillator
+   *
+   * @param {number} speed - wave period
+   * @param {number} depth - wave amplitude
+   *
+   * @return {number}
+   */
+  tanOscillator = (speed = 1, depth = 1) =>
+  {
+    return Math.tan(this.counter * speed) * depth
+  }
 }
 
 /**
- * Tangent wave oscillator
- *
- * @param {number} speed - wave period
- * @param {number} depth - wave amplitude
- *
- * @return {number}
+ * Public main class
  */
-const tanOscillator = (speed = 1, depth = 1) => {
-  return Math.tan(counter * speed) * depth
-}
+class AlienArpeggios
+{
+  constructor()
+  {
+    this.configuration = new AlienArpeggiosConfiguration;
+    this.helper        = new AlienArpeggiosHelper(this.configuration);
+  }
 
-/**
- * Simple arpeggios using the lower limit as target note
- * 
- * @param {number} speed - wave period
- * @param {number} depth - wave amplitude
- * 
- * @return {function}
- */
-const simpleArpeggios = (speed = 1, depth = 1, targetNote = lowerNote) => {
-  return setInterval(() => {
+  /**
+   * Simple arpeggios using the lower limit as target note
+   * 
+   * @param {number} speed - wave period
+   * @param {number} depth - wave amplitude
+   * 
+   * @return {function}
+   */
+  simpleArpeggios = (speed = 1, depth = 1, targetNote = lowerNote) =>
+  {
+    return setInterval(() => {
 
-    playNote(lowerNote + tanOscillator(speed, depth));
+      this.helper.playNote(
+        this.configuration.lowerNote + this.helper.tanOscillator(speed, depth)
+      );
+      
+      this.helper.counter++;
     
-    counter++;
-  
-  }, 100);
-}
+    }, 100);
+  }
 
-/**
- * Play function for duration
- *
- * @param {number} speed 
- * @param {number} depth 
- * @param {number} targetNote - MIDI note from 0 to 127
- * @param {number} duration - in milliseconds
- * @param {function} callback - simpleArpeggios or the like
- */
-const playFor = (speed, depth, targetNote, duration = 1000, callback = () => {}) => {
-  let callbackFunction = callback(speed, depth, targetNote);
+  /**
+   * Play function for duration
+   *
+   * @param {number} speed 
+   * @param {number} depth 
+   * @param {number} targetNote - MIDI note from 0 to 127
+   * @param {number} duration - in milliseconds
+   * @param {function} callback - simpleArpeggios or the like
+   */
+  playFor = (speed, depth, targetNote, duration = 1000, callback = () => {}) =>
+  {
+    let callbackFunction = callback(speed, depth, targetNote);
 
-  setTimeout(() => {
+    setTimeout(() => {
 
-    clearInterval(callbackFunction);
-  
-  }, duration);
+      clearInterval(callbackFunction);
+    
+    }, duration);
+  }
 }
 
 /**
  * Creative block
  */
-setInterval(() => {
+let player   = new AlienArpeggios();
+let interval = 3000;
 
-  let targetNote = Math.floor(Math.random() * upperNote + lowerNote);
-  console.log("Setting target note to "+targetNote);
+console.log("Playing something every " + interval + "milliseconds");
 
-  playFor(0.2, 3, targetNote, 3000, simpleArpeggios);
+setInterval(() =>
+{
+  let targetNote = Math.floor(
+    Math.random() * player.configuration.upperNote + player.configuration.lowerNote
+  );
 
-}, 3000)
+  console.log("Setting target note to " + targetNote);
+
+  player.playFor(0.2, 3, targetNote, 3000, player.simpleArpeggios);
+
+}, interval)
